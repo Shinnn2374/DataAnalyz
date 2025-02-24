d13f95c4f07b39d5fcf6c1b340667497ec598bd1
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-02-24 19:42:34 +0300

    ...

--- Data/Analysis/Analis.py
+++ Data/Analysis/Analis.py
@@ -9,13 +9,29 @@ data_copy = data.copy()
 
 # Группировка данных по профессиям и расчет средней ожидаемой зарплаты
 avg_salary_by_profession = data.groupby('profession')['expected_salary'].mean().sort_values(ascending=False)
-print(f'Группировка данных по профессиям и расчет средней ожидаемой зарплаты  {avg_salary_by_profession}')
+print(f'Группировка данных по профессиям и расчет средней ожидаемой зарплаты: {avg_salary_by_profession}')
+print('_____________________________________________________________________')
 
 # Визуализация
 plt.figure(figsize=(12, 8))
 avg_salary_by_profession.head(20).plot(kind='bar', color='skyblue')  # Топ-20 профессий
 plt.title('Средняя ожидаемая зарплата по профессиям', fontsize=16)
 plt.xlabel('Профессия', fontsize=14)
+plt.ylabel('Средняя зарплата', fontsize=14)
+plt.xticks(rotation=45, ha='right')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.tight_layout()
+plt.show()
+
+# Группировка данных по регионам и расчет средней зарплаты
+avg_salary_by_region = data.groupby('resume_region')['expected_salary'].mean().sort_values(ascending=False)
+print(f'Группировка данных по регионам и расчет средней зарплаты: {avg_salary_by_region}')
+
+# Визуализация
+plt.figure(figsize=(12, 8))
+avg_salary_by_region.head(20).plot(kind='bar', color='lightgreen')  # Топ-20 регионов
+plt.title('Средняя ожидаемая зарплата по регионам', fontsize=16)
+plt.xlabel('Регион', fontsize=14)
 plt.ylabel('Средняя зарплата', fontsize=14)
 plt.xticks(rotation=45, ha='right')
 plt.grid(axis='y', linestyle='--', alpha=0.7)
