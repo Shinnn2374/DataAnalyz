1823fb51f3da8e7cac2d7669c2760e514966a374
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-02-24 19:34:34 +0300

    ...

---
+++ Data/Analysis/Analis.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv('cleaned_hh_ru_dataset.csv')
+data_copy = data.copy()
--- Data/CleanData.py
+++ Data/CleanData.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # Загрузка датасета
-df = pd.read_csv('Analysis/datas/hh_ru_dataset.csv')
+df = pd.read_csv('hh_ru_dataset.csv')
 
 # Удаление дубликатов
 df.drop_duplicates(inplace=True)
