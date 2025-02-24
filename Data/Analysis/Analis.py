cc151963439c10acaded1a82e38f3f2b45fe40e7
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-02-24 19:51:49 +0300

    ...

--- Data/Analysis/Analis.py
+++ Data/Analysis/Analis.py
@@ -55,3 +55,22 @@ plt.xticks(rotation=45, ha='right')
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.tight_layout()
 plt.show()
+
+# Расчет средней вилки зарплат (compensation_from и compensation_to)
+data['average_compensation'] = (data['compensation_from'] + data['compensation_to']) / 2
+
+# Группировка по профессиям и расчет средней вилки зарплат
+avg_compensation_by_profession = data.groupby('profession')['average_compensation'].mean().sort_values(ascending=False)
+print(f'Группировка по профессиям и расчет средней вилки зарплат: {avg_compensation_by_profession}')
+print('_____________________________________________________________________')
+
+# Визуализация
+plt.figure(figsize=(12, 8))
+avg_compensation_by_profession.head(20).plot(kind='bar', color='purple')  # Топ-20 профессий
+plt.title('Средняя вилка зарплат по профессиям', fontsize=16)
+plt.xlabel('Профессия', fontsize=14)
+plt.ylabel('Средняя вилка зарплат', fontsize=14)
+plt.xticks(rotation=45, ha='right')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.tight_layout()
+plt.show()
